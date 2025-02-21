edba6d8dca30d69ab46e6535c3e5f27d6d553754
namkyu-kim-svg <neb.nkkim@gmail.com>
2025-02-21 14:29:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import matplotlib as mpl  # rc 함수 사용을 위한 라이브러리 추가
 import os
 
 # ✅ 맑은고딕 폰트 설정
-font_path = "./MALGUN.TTF"  # 업로드한 폰트 파일 경로
+font_path = "MALGUN.TTF"
 if os.path.exists(font_path):
     font = fm.FontProperties(fname=font_path).get_name()
     mpl.rc('font', family=font)  # 전체적으로 폰트 적용
