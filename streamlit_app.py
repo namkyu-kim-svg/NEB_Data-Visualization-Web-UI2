b6cdfcaf610966acca6930d4dc23c0e4d13dc004
namkyu-kim-svg <neb.nkkim@gmail.com>
2025-02-21 13:56:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,10 @@ if menu == "홈":
         """,
         unsafe_allow_html=True
     )
-    # 첫 화면 이미지 표시
-    # 첫 화면 이미지 표시
-    image_path = r"C:\Users\Neoenbiz\Desktop\데이터 시각화 웹 브라우저 자료\첫 화면.png"
-    st.image(image_path, caption="데이터 시각화 웹 앱", use_container_width=True)
+# 첫 화면 이미지 표시
+image_url = "https://raw.githubusercontent.com/namkyu-kim-svg/main/main/첫%20화면.png"
+st.image(image_url, caption="데이터 시각화 웹 앱", use_container_width=True)
+
 
 
 # **5. 그래프 페이지**
