425588f21f2aba7d037afd0c6c8e36b5d4e741d3
namkyu-kim-svg <neb.nkkim@gmail.com>
2025-02-25 08:51:00 +0900

    Update Map.py

--- Map.py
+++ Map.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title("지도 페이지")
-st.header("지도 페이지")
-st.write("지도를 구현할 라이브러리를 선택하여 추가할 수 있습니다.")
+def show():
+    st.title("지도 페이지")
+    st.header("지도 페이지")
+    st.write("지도를 구현할 라이브러리를 선택하여 추가할 수 있습니다.")
+
+if __name__ == '__main__':
+    show()
