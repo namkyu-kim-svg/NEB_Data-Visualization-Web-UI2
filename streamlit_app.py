31ac69f5fc26ba444a05423c5198476a0f7019f8
namkyu-kim-svg <neb.nkkim@gmail.com>
2025-02-28 10:54:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,27 +16,3 @@ elif page == "통계 분석":
 elif page == "지도":
     show_map()
     
-    # 하단 레이아웃 구성
-    st.write("---")  # 구분선
-    
-    # GitHub에 올라간 py50logo.png 로드 (raw 파일 URL 사용)
-    logo_url = "https://raw.githubusercontent.com/namkyu-kim-svg/NEB_Data-Visualization-Web-UI2/main/py50logo.png"
-
-    # 2개 컬럼으로 나누어 왼쪽은 로고, 오른쪽은 문구
-    col1, col2 = st.columns([1, 4])  # 비율 조정 가능
-
-    with col1:
-        st.image(logo_url, width=80)
-
-    with col2:
-        # 오른쪽 정렬 + 원하는 문구
-        st.markdown(
-            """
-            <p style="text-align: right; font-size:16px; font-weight:bold; margin-top:20px;">
-            내 Streamlit 앱 보기 → Py50: Generate Dose-Response Curve 활용
-            </p>
-            """,
-            unsafe_allow_html=True
-        )
-
-
