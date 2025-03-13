4e0dcd5022e32f7741a1c817e6081b6bcc98e523
namkyu-kim-svg <neb.nkkim@gmail.com>
2025-03-13 12:59:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,15 @@ from Statistics import show as show_statistics
 from Map import show as show_map
 from streamlit_option_menu import option_menu  # streamlit-option-menu 추가
 
-# 왼쪽 사이드바에 메뉴 추가
-selected = option_menu(
-    "메뉴",                # 메뉴 제목
-    ["홈", "그래프", "통계 분석", "지도"],  # 메뉴 항목
-    icons=["house", "bar-chart", "calculator", "map"],  # 각 항목의 아이콘
-    menu_icon="cast",      # 전체 메뉴 아이콘
-    default_index=0,       # 기본 선택 항목 (인덱스)
-    orientation="vertical"  # 메뉴를 세로로 표시
-)
+# 사이드바에 메뉴 배치
+with st.sidebar:
+    selected = option_menu(
+        "메뉴",                         # 메뉴 제목
+        ["홈", "그래프", "통계 분석", "지도"],  # 메뉴 항목
+        icons=["house", "bar-chart", "calculator", "map"],  # 각 항목의 아이콘
+        menu_icon="cast",               # 전체 메뉴 아이콘
+        default_index=0,                # 기본 선택 항목
+    )
 
 # 메뉴 선택에 따라 페이지 함수 실행
 if selected == "홈":
